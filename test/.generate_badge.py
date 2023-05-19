a4ddd2adba804a982d0af94fd0113d98528f7701
vaishakp <vaishakprasad@gmail.com>
2023-05-19 23:37:52 +0530

    typo in gen_badge

--- test/.generate_badge.py
+++ test/.generate_badge.py
@@ -8,7 +8,7 @@ total_cov = cov.report()
 badge = Image.new("RGB", (140, 60))
 font = ImageFont.load_default()
 
-bd = ImageDraw.Draw(im)
+bd = ImageDraw.Draw(badge)
 
 bd.text((10, 5), "coverage:", fill=(255, 255, 255), font=font)
 bd.rectangle([(80, 0), (150, 20)], fill=(220, 0, 0))
